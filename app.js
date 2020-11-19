86e11952ecd2f9d37ac8d4a2e29293f1e564147c
Yurii <vasilvasilovichvasil@gmail.com>
2020-11-19 16:09:58 +0200

    Play Again and Random right answer added

--- app.js
+++ app.js
@@ -6,32 +6,56 @@ const guessInput = document.querySelector('.guessInput')
 const guessBtn = document.querySelector('.guessSubmit')
 const message = document.querySelector('.message')
 //Game vars
+
+generateRightNum = (min, max) => {
+    return (Math.floor(Math.random() * (max - min + 1) + 1))
+}
 let min = 1,
     max = 10,
-    rightNum = 7,
-    guessLeft = 3
+    rightNum = generateRightNum(min, max),
+    guesesLeft = 3
 
 minNum.textContent = min
 maxNum.textContent = max
 
+game.addEventListener('mousedown', (event) => {
+    if (event.target.classList.contains('playAgain')){
+        window.location.reload()
+    }
+})
 guessBtn.addEventListener('click',() => {
     let guess = Number(guessInput.value)
 
-    if (isNaN(guess) || guess < min || guess > max){
-        setMessage(`Please write number between ${min} and ${max}`, 'red')
-    }
-    debugger
-    if (guess === rightNum){
-        console.log(7)
-        guessInput.disabled = true
-        guessInput.style.borderColor = 'green'
-        setMessage(`${rightNum} is correct!`, 'green')
-    }else{
+    if (isNaN(guess) || guess < min || guess > max){setMessage(`Please write number between ${min} and ${max}`, 'red')}
+    else {
+        if (guess === rightNum){
+            //Game Won
+            gameOver(true, `${rightNum} is correct!`)
+        }else{
+            //Wrong number
+            guesesLeft -= 1
 
+            if(guesesLeft === 0){
+                //Game over
+                gameOver(false, `GAME OVER. The correct answer was ${rightNum}`)
+            }else {
+                //Game continue - answer wrong
+                guessInput.style.borderColor = 'yellow'
+                guessInput.value = ''
+                setMessage(`${guess} is not correct, ${guesesLeft} tries left`, 'yellow')
+            }
+        }
     }
 })
-
-setMessage = (msg, color) => {
+gameOver = (isWin, msg) => {
+    let color = isWin ? 'green' : 'red'
+    guessInput.disabled = true
+    guessInput.style.borderColor = color
+    setMessage(msg, color)
+    guessBtn.value = 'Play Again'
+    guessBtn.className += ' playAgain'
+}
+setMessage = (msg, color = 'black') => {
     message.textContent = msg
     message.style.color = color
 }
